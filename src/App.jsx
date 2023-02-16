3c49ec918be69715a25d7aa126b29764d380c7fb
Jorge <jorlopeztohme@gmail.com>
2023-02-16 15:22:27 -0300

    Add LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 import Editor from '@monaco-editor/react'
@@ -11,17 +11,36 @@ import Menu from './Components/Menu/Menu.jsx'
 import Result from './Components/Result/Result.jsx'
 
 function App () {
-  const [html, setHtml] = useState('')
-  const [css, setCss] = useState('')
-  const [js, setJS] = useState('')
+  // Get data from LocalStorage
+  let htmlStored = ''
+  let cssStored = ''
+  let jsStored = ''
+
+  if (localStorage.getItem('data')) {
+    const data = JSON.parse(localStorage.getItem('data'))
+    htmlStored = data.html
+    cssStored = data.css
+    jsStored = data.js
+  } // If there is no data in LocalStorage, the value is empty
+
+  const [html, setHtml] = useState(htmlStored)
+  const [css, setCss] = useState(cssStored)
+  const [js, setJS] = useState(jsStored)
 
   const { handleHTML, handleCSS, handleJS } = useChange(setHtml, setCss, setJS)
 
   const [distribution, setDistribution] = useState('distribution-1')
 
+  // Change distribution
   const handleDistribution = (e) => {
     setDistribution(e.target.value)
   }
+
+  // Save data in LocalStorage
+  useEffect(() => {
+    const data = { html, css, js }
+    localStorage.setItem('data', JSON.stringify(data))
+  }, [html, css, js])
 
   return (
     <div className='App'>
@@ -34,6 +53,7 @@ function App () {
             theme='vs-dark'
             options={{ fontSize: 20, minimap: { enabled: false } } }
             id='html'
+            value={html}
           />
         </div>
         <div className='box css'>
@@ -43,6 +63,7 @@ function App () {
             theme='vs-dark'
             options={{ fontSize: 20, minimap: { enabled: false } } }
             id='css'
+            value={css}
           />
         </div>
         <div className='box js'>
@@ -52,6 +73,7 @@ function App () {
             theme='vs-dark'
             options={{ fontSize: 20, minimap: { enabled: false } } }
             id='js'
+            value={js}
           />
         </div>
         <div className='box result'>
